28d553fc4cbd14106f85ebb1dfd59a397a32cac6
Razmik Kocharyan <razmikkocharian@gmail.com>
2023-12-20 14:45:58 +0400

    added BookForm BookList Form components

---
+++ frontend/src/components/Filter/Filter.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Filter.css'
+
+const Filter = () => {
+  return (
+    <div className='app-block filter'>
+        <h2>Filters</h2>
+    </div>
+  )
+}
+
+export default Filter
